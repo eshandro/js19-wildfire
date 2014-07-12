1bfafe1ad6505902e3e6544fe5671a3eb2886d6c
eshandro <erik843@gmail.com>
2014-07-12 17:49:02 -0600

    Added filter for btn already on fire

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var lakeSelected = false;
 var campfireSelected = false;
 
+// Helper function that finds the btn class of a targeted li
+var findBtnClass = function (target) {
+	return $(target).attr('class');
+};
+
+
+
 var catchFire = function(clicked) {
 	// Get index of clicked
 	var currentIndex = $(clicked).parent().index();
@@ -9,35 +16,59 @@ var catchFire = function(clicked) {
 	if (currentIndex !== 0 && currentIndex !== 9 && currentIndex !== 18 && currentIndex !==27 && 
 		currentIndex !== 36 && currentIndex !== 45 && currentIndex !== 54 && currentIndex !== 63 && 
 		currentIndex !== 72) {
-			$(clicked).parent().prev().children().removeClass('btn-default').removeClass('btn-info');
-			$(clicked).parent().prev().children().addClass('btn-warning');
+		
+		var targetedLeftBtn = $(clicked).parent().prev().children();
+		// Tests to make sure btn isn't already on fire	
+		if(findBtnClass(targetedLeftBtn) !== 'btn btn-warning' && 
+			findBtnClass(targetedLeftBtn) !== 'btn btn-danger') {
+
+			$(targetedLeftBtn).removeClass('btn-default').removeClass('btn-info');
+			$(targetedLeftBtn).addClass('btn-warning');
 		}
+	}
 	// Test for edge to set fire to right side
 	if (currentIndex !== 8 && currentIndex !== 17 && currentIndex !== 26 && currentIndex !==35 &&
 		currentIndex !== 44 && currentIndex !== 53 && currentIndex !== 62 && currentIndex !== 71 &&
 		currentIndex !== 80) {
-			$(clicked).parent().next().children().removeClass('btn-default').removeClass('btn-info');
-			$(clicked).parent().next().children().addClass('btn-warning');
-		}
+		var targetedRightBtn = $(clicked).parent().next().children();
+		// Tests to make sure targeted btn isn't already on fire
+		if(findBtnClass(targetedRightBtn) !== 'btn btn-warning' && 
+			findBtnClass(targetedRightBtn) !== 'btn btn-danger') {
+
+			$(targetedRightBtn).removeClass('btn-default').removeClass('btn-info');
+			$(targetedRightBtn).addClass('btn-warning');
+		}	
+	}
 	
 	// Test if above exists and set fire
 	if (currentIndex > 8) {
-
 		var temp = 'li:eq(' + (currentIndex-9) + ')';
-		$(temp).children().removeClass('btn-default').removeClass('btn-info');
-		$(temp).children().addClass('btn-warning');
+		var targetedTopBtn = $(temp).children();
+
+		// Test to make sure targeted btn isn't already on fire
+		if (findBtnClass(targetedTopBtn) !== 'btn btn-warning' && 
+			findBtnClass(targetedTopBtn) !== 'btn btn-danger') {
+			$(targetedTopBtn).removeClass('btn-default').removeClass('btn-info');
+			$(targetedTopBtn).addClass('btn-warning');
+		}
 	}
 
 	// Test if below exists and set fire
 	if (currentIndex < 72) {
 		var temp = 'li:eq(' + (currentIndex + 9) + ')';
-		$(temp).children().removeClass('btn-default').removeClass('btn-info');
-		$(temp).children().addClass('btn-warning');
+		var targetedBottomBtn = $(temp).children();
+		// Tests if targeted btn is alrady on fire
+		if (findBtnClass(targetedBottomBtn) !== 'btn btn-warning' && 
+			findBtnClass(targetedBottomBtn) !== 'btn btn-danger') {
+			$(targetedBottomBtn).removeClass('btn-default').removeClass('btn-info');
+			$(targetedBottomBtn).addClass('btn-warning');
+		}
 	}
 };
 
 var spreadFire = function() {
 	setInterval(function () {
+		// Loops thru all li's
 		for(var i=0; i < $('li').length; i++) {
 			var temp = 'li:eq(' + i + ')';
 			if($(temp).children().attr('class') === 'btn btn-warning') {
@@ -45,7 +76,7 @@ var spreadFire = function() {
 				catchFire(onFire);
 			}
 		}
-	}, 2000);
+	}, 4000);
 };
 
 $(document).on('ready', function() {
